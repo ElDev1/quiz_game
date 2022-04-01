3c8dfe4056a4c5cd5aff92b6d2f8defb8dd07c52
Devi Amaolo <deviamaolo@gmail.com>
2022-04-01 01:58:51 -0300

    adding random data questions

---
+++ data/data.js
@@ -0,0 +1,23 @@
+const data = [
+    {
+        question: 'whats your name',
+        choices: [
+            'juan',
+            'martin',
+            'fernando',
+            'lolo'
+        ],
+        answer: 'lolo'
+    },
+    {
+        question: 'whats my name',
+        choices: [
+            'fede',
+            'lucas',
+            'gonzalo',
+            'lalo'
+        ],
+        answer: 'lalo'
+    },
+
+]
